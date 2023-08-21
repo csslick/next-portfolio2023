5d5983a6ee1770273e09b26a3f0cb4b1aaee0bbf
csslick <digitone@naver.com>
2023-08-21 14:18:43 +0900

    detail

--- src/components/ProjectItem.jsx
+++ src/components/ProjectItem.jsx
@@ -1,12 +1,14 @@
+import Link from 'next/link'
+
 export default function ProjectItem(props) {
-  const { title, image } = props; // 1
+  const { title, image, id } = props;
 
   return (
     <div className="project-item">
-      <a href="#" target="_blank">
+      <Link href={`/detail/${id}`}>
         <img src={image} alt={title} />
         <h3>{title}</h3>
-      </a>
+      </Link>
     </div>
   )
 }
